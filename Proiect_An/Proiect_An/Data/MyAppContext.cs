df0807637ae07eccf017c7bcd10cf523506efadf
alexolaras <alex.olaras11@gmail.com>
2025-05-26 19:57:26 +0300

    added conections to the database, implemented crud operations for the item

---
+++ Proiect_An/Proiect_An/Data/MyAppContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Proiect_An.Models;
+
+namespace Proiect_An.Data
+{
+    public class MyAppContext : DbContext
+    {
+        public MyAppContext(DbContextOptions<MyAppContext> options): base(options)
+        {
+
+        }
+        public DbSet<Item> Items { get; set; }
+    }
+}
--- Proiect_An/Proiect_An/Program.cs
+++ Proiect_An/Proiect_An/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Proiect_An.Data;
+
 namespace Proiect_An
 {
     public class Program
@@ -8,6 +11,7 @@ namespace Proiect_An
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<MyAppContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
 
             var app = builder.Build();
 
