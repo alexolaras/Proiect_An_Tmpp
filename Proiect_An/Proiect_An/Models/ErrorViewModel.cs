ebc00f3da6a261f4fff67d56bc2ac23e870a60f8
alexolaras <alex.olaras11@gmail.com>
2025-05-26 15:55:05 +0300

    initial code structure

---
+++ Proiect_An/Proiect_An/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Proiect_An.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
